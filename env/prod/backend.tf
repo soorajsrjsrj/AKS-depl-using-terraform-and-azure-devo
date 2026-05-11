81f07d1f4814d1a0b3ca824d8dd3b83476bcbae3
Sooraj S <soorajsuneesh@gmail.com>
2026-05-10 20:09:38 -0400

    Fix storage account name in backend.tf

--- env/prod/backend.tf
+++ env/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "NCPL"
-    storage_account_name = "prod1storagecentralus"
+    storage_account_name = "prod1storagecentralu1"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
