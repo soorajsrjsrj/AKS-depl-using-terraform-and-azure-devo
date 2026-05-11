0242a98c12171b354071c73ff3e9d6e2ec83d788
Sooraj S <soorajsuneesh@gmail.com>
2026-05-10 20:12:24 -0400

    fix backend storage name

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "NCPL"
-    storage_account_name = "dev1storagecanadacentral"
+    storage_account_name = "dev1storagecanadacentra1"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
